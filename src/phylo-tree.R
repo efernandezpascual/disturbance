934f634bc98fadc33b8e0377ea897b912b8cb2bf
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2024-05-28 14:24:45 +0200

    dataset representativeness

--- src/phylo-tree.R
+++ src/phylo-tree.R
@@ -6,15 +6,11 @@ rm(list = ls())
 ### Study species
 
 read.csv("data/disturbance-indicators.csv", fileEncoding = "latin1") %>%
-  select(species, family) %>%
+  select(species) %>%
   unique %>%
   separate(species, into = c("genus", "epithet"), sep = " ", remove = FALSE) %>%
-  select(species, genus, family) %>%
+  select(species, genus) %>%
   unique %>%
-  mutate(family = fct_recode(family, 
-                             "Asteraceae" = "Compositae",
-                             "Fabaceae" = "Leguminosae",
-                             "Asphodelaceae" = "Xanthorrhoeaceae")) %>%
   arrange(species) %>%
   na.omit %>%
   mutate(species.relative = ifelse(species == "Soda inermis", "Salsola soda", "")) %>%
@@ -38,12 +34,6 @@ phylo.maker(ranks1,
             scenario = 3) ->
   tree
 
-### Tree match stats
-
-tree$sp.list %>%
-  group_by(output.note) %>% tally
-
 ### Save
 
-write.csv(tree$sp.list, "results/phylo-tree/output_splist.csv", fileEncoding = "latin1", row.names = FALSE)
-write.tree(tree$phylo, "results/phylo-tree/disturbance.tre")
+write.tree(tree, "results/phylo-tree/disturbance.tre")
